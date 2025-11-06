da64e0fab179aa859a8df0bc7a6c6c984196cdba
kiyimba_henry <kiyimbahenry314@gmail.com>
2025-11-06 13:56:58 +0300

    Update main

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,7 +8,12 @@ interface Teacher {
     [propName: string]: any;
 }
 
-// Example implementation
+// Directors interface that extends Teacher
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+// Example implementation for Teacher
 const teacher3: Teacher = {
     firstName: 'John',
     lastName: 'Doe',
@@ -18,3 +23,14 @@ const teacher3: Teacher = {
 };
 
 console.log('Teacher Object:', teacher3);
+
+// Example implementation for Directors
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+
+console.log('Director Object:', director1);
