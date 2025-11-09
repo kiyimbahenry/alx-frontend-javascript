768cecef318a6786f3e5d8c5a41d4d9096bda3ac
kiyimba_henry <kiyimbahenry314@gmail.com>
2025-11-09 08:55:45 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,49 +1,35 @@
-// Teacher interface definition
-interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [propName: string]: any;
+// Interface for the constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
 }
 
-// Director interface that extends Teacher
-interface Director extends Teacher {
-    numberOfReports: number;
+// Interface for the class instance
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
 }
 
-// Interface for printTeacher function
-interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
+// Class implementation
+class StudentClass implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
 
-// printTeacher function implementation
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-    return `${firstName.charAt(0)}. ${lastName}`;
-};
+// This ensures the constructor matches the interface
+const StudentClassConstructor: StudentConstructor = StudentClass;
 
-// Example implementation for Teacher
-const teacher3: Teacher = {
-    firstName: 'John',
-    lastName: 'Doe',
-    fullTimeEmployee: false,
-    location: 'London',
-    contract: false,
-};
-
-console.log('Teacher Object:', teacher3);
-
-// Example implementation for Director
-const director1: Director = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-};
-
-console.log('Director Object:', director1);
-
-// Test the printTeacher function
-console.log('printTeacher result:', printTeacher("John", "Doe"));
+// Export for use in other files if needed
+export { StudentClass, StudentConstructor, StudentInterface };
